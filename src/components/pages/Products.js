81262d4d36fb8e198585fc1d5ae6b50f246bcd4a
Ashley Pliler <ashleypliler1@gmail.com>
2023-04-22 19:13:18 -0600

    caruosel

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -9,11 +9,17 @@ import { HomeCarousel } from "../carousel/HomeCarousel";
 import slide1 from '../../images/slide1.png'
 import slide2 from '../../images/slide2.png'
 import slide3 from '../../images/slide3.png'
+import slide4 from '../../images/slide4.png'
+import slide5 from '../../images/slide5.png'
+import slide6 from '../../images/slide6.png'
 
 const slides = [
   slide1,
   slide2,
   slide3,
+  slide4,
+  slide5,
+  slide6
 ];
 
 
